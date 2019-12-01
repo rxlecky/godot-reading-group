75766022cd06c73ef6a16c050de0af8f3cf9a4dd
AJ Weeks <ajweeks@shaw.ca>
2019-12-01 18:17:06 +0000

    Update slides

---
+++ addons/scene_notes/Timer4.gd
@@ -0,0 +1,15 @@
+extends Label
+
+var startTime : float
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var now := OS.get_system_time_msecs()
+	var elapsed = now - startTime
+	
+	text = String(elapsed/1000.0)
+
+
+func _on_Button_pressed():
+	startTime = OS.get_system_time_msecs()
+	
